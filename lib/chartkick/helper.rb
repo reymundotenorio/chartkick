5cbe68407d9c5eb06f73d9402cf93c6725f360c7
Reymundo Tenorio <reymundotenorio@gmail.com>
2018-12-28 21:10:13 +0000

    Adding support with Turbolinks 5 v1

--- lib/chartkick/helper.rb
+++ lib/chartkick/helper.rb
@@ -68,19 +68,8 @@ module Chartkick
         js = <<JS
 <script type="text/javascript"#{nonce_html}>
 
-  (function() {
-    var createChart = function() { #{createjs} };
-    if (window.addEventListener) {
-      window.addEventListener("load", createChart, true);
-    } else if (window.attachEvent) {
-      window.attachEvent("onload", createChart);
-    } else {
-      createChart();
-    }
-  })();
-      
   $(document).on("turbolinks:load", function(){
-    createChart();
+    #{createjs}
   });
 
 </script>
